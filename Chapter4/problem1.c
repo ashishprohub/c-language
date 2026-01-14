5f89e356cb491cf10e147e79326c97ac5018f13a
ashishprohub <ashish11072005@gmail.com>
2026-01-14 10:22:15 +0530

    Problem 1 solved and Break

--- Chapter4/problem1.c
+++ Chapter4/problem1.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 
 int main(){
-    
+    int table;
+    printf("Enter the number to get table for:",table);
+    scanf("%d",&table);
+    for(int i =1; i <= 10; i++){
+        printf("%d X %d = %d\n", table, i, table*i);
+    }
     return 0;
 }
