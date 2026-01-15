ac867f4fa6600d5a00fabfbb18fc82dcd3d4a1bb
ashishprohub <ashish11072005@gmail.com>
2026-01-15 17:08:00 +0530

    Problem 11 solved

--- Chapter4/problem11.c
+++ Chapter4/problem11.c
@@ -1,1 +1,29 @@
 //  Implement 10 using other types of loops. 
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int n) {
+    if (n <= 1) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+    int i = 1;
+    while(i <= sqrt(n)){
+        if (n % i == 0){
+            return 0;            
+        }
+        i +=2;
+    }
+    return 1;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+    return 0;
+}
