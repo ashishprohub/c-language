9a65cb4bdb951056a55d03c4c13e818a8e5b13be
ashishprohub <ashish11072005@gmail.com>
2026-01-15 15:31:10 +0530

    Problem 7 done

--- Chapter4/problem7.c
+++ Chapter4/problem7.c
@@ -2,3 +2,17 @@
 Write a program to calculate the sum of the numbers occurring in the 
 multiplication table of 8. (consider 8 x 1 to 8 x 10).
 */
+#include <stdio.h>
+
+int main(){
+    int i = 1;
+    int sum = 0;
+    int j;
+    printf("Enter the number whose sum of table to find:",j);
+    scanf("%d",&j);
+    for(int i =1; i <= 10; i++){
+        sum +=i;
+    }
+    printf("Sum of table of %d is %d",j,sum*j);
+    return 0;
+}
