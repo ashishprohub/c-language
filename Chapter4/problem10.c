c7876259f29d54ee5d9cd24e6a8e02ac48885160
ashishprohub <ashish11072005@gmail.com>
2026-01-15 16:59:19 +0530

    problem 10 done

--- Chapter4/problem10.c
+++ Chapter4/problem10.c
@@ -1,1 +1,27 @@
 // Write a program to check whether a given number is prime or not using loops.
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int n) {
+    if (n <= 1) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (isPrime(num)) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+    return 0;
+}
