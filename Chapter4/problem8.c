f54ff52a64fb2c9f3b0a25c0bb96482a8e9cb5a9
ashishprohub <ashish11072005@gmail.com>
2026-01-15 15:58:56 +0530

    n

--- Chapter4/problem8.c
+++ Chapter4/problem8.c
@@ -1,1 +1,15 @@
 // Write a program to calculate the factorial of a given number using a for loop. 
+#include <stdio.h>
+
+int main(){
+    int i;
+    int j =1;
+    int fact = 1;
+    printf("Write number to find its factorial:",i);
+    scanf("%d",&i);
+    for(int j =1; j <= i; j++){
+        fact *=j;
+    }
+    printf("Factorial of %d is %d",i,fact);
+    return 0;
+}
