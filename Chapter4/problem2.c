58de768cbd3dab8c9f1908a39853b1ea89f92890
ashishprohub <ashish11072005@gmail.com>
2026-01-15 14:11:53 +0530

    prob2 solved

--- Chapter4/problem2.c
+++ Chapter4/problem2.c
@@ -1,1 +1,11 @@
 // Write a program to print multiplication table of 10 in reversed order.
+#include <stdio.h>
+
+int main(){
+    int m =10;
+    int i;
+    for(i = 10; i>=1; i--){
+    printf("%d X %d = %d\n",m,i,m*i);
+    }
+    return 0;
+}
