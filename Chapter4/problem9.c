77f32b30841a6c9ff97607ec99c81b96bd1197bc
ashishprohub <ashish11072005@gmail.com>
2026-01-15 16:11:58 +0530

    problem 9 done

--- Chapter4/problem9.c
+++ Chapter4/problem9.c
@@ -1,1 +1,17 @@
 // Repeat 8 using while loop.
+#include <stdio.h>
+
+int main(){
+    int i;
+    int j =1;
+    int fact = 1;
+    printf("Write number to find its factorial:",i);
+    scanf("%d",&i);
+    while(j<=i)
+    {
+        fact *=j;
+        j++;
+    }
+    printf("Factorial of %d is %d",i,fact);
+    return 0;
+}
