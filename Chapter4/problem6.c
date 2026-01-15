e792c9724b4d4d9fe9e5525c32d1a4213b0a7791
ashishprohub <ashish11072005@gmail.com>
2026-01-15 15:22:16 +0530

    Problem 6 done..

--- Chapter4/problem6.c
+++ Chapter4/problem6.c
@@ -1,1 +1,21 @@
 // Write a program to implement program 5 using ‘for’ and ‘do-while’ loop.
+#include <stdio.h>
+
+int main(){
+    int i =1;
+    int sum = 0;
+    for( i =1; i <= 10; i++){
+        sum +=i;
+    }
+    printf("Sum of first ten natural numbers is %d",sum);
+    // For do while
+    // int i =1;
+    // int sum =0;
+    // do{
+    //     sum += i;
+    //     i ++;
+    // }
+    // while( i <= 10);
+    // printf("Sum od first ten natural numbers is %d",sum);
+    return 0;
+}
