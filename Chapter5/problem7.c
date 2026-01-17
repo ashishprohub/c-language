f7df0d9c37f2d4f0875042303d39b3f6afacaa7a
ashishprohub <ashish11072005@gmail.com>
2026-01-17 08:07:03 +0530

    problem7 solved

--- Chapter5/problem7.c
+++ Chapter5/problem7.c
@@ -1,1 +1,15 @@
-// Write a program using function to print the following pattern (first n lines)
+// Write a program using function to print the following pattern (first n lines).
+#include <stdio.h>
+
+int main(){
+    int n;
+    printf("Enter the number:",n);
+    scanf("%d",&n);
+    for (int i =0; i<=n; i++){
+        for(int j =0; j<i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
