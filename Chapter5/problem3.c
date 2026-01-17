89fc68d05f74962484f150f76b0ebf8a9f8bb0da
ashishprohub <ashish11072005@gmail.com>
2026-01-17 07:30:03 +0530

    problem 3 solved

--- Chapter5/problem3.c
+++ Chapter5/problem3.c
@@ -1,2 +1,15 @@
-// Write a function to calculate force of attraction on a body of mass ‘m’ exerted by 
-// earth. Consider g = 9.8m/s2. 
+/*Write a function to calculate force of attraction on a body of mass ‘m’ exerted by 
+earth. Consider g = 9.8m/s2. */
+// F = m.a
+#include <stdio.h>
+    float force(int m);
+    float force(int m){
+        return m*9.8;
+    }
+int main(){
+    int m;
+    printf("Enter the mass of body(in Kg): ",m);
+    scanf("%d",&m);
+    printf("Force exerted on mass %d Kg is %f N",m,force(m));
+    return 0;
+}
