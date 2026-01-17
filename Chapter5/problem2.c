e1392b5eee25b9828776885b7d421aa91ea4dd51
ashishprohub <ashish11072005@gmail.com>
2026-01-17 07:24:23 +0530

    problem2 solved

--- Chapter5/problem2.c
+++ Chapter5/problem2.c
@@ -1,1 +1,14 @@
 // Write a function to convert Celsius temperature into Fahrenheit. 
+#include <stdio.h>
+    float df(int c);
+    float df(int c){
+        return((c*9.0)/5.0)+32.0;
+    }
+
+int main(){
+    int c;
+    printf("Enter the temp in celsius: ",c);
+    scanf("%d",&c);
+    printf("Temperature in degree fahrenheit is: %f",df(c));
+    return 0;
+}
