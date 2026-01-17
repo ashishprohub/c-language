2ee28041ba2cf6573434f203fcf4522a30e999fb
ashishprohub <ashish11072005@gmail.com>
2026-01-17 07:50:01 +0530

    problem 5 sort out

--- Chapter5/problem5.c
+++ Chapter5/problem5.c
@@ -1,3 +1,12 @@
 // // What will the following line produce in a C program: 
 // int a = 4; 
 // printf("%d %d %d \n", a, ++a, a++); 
+// 4,5,5 (Because evaluation order is not defined here so compiler automatically determined the order
+// that is right to left therefore showing there 6,6,4
+#include <stdio.h>
+
+int main(){
+    int a = 4; 
+    printf("%d %d %d \n", a, ++a, a++); 
+    return 0;
+}
